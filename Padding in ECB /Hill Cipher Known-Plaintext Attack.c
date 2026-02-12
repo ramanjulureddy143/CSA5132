cf592ed3b9ab34036ebd2e6b34d0d7c4e3957f31
ramanjulureddy143 <ramanjulareddyu2123.sse@saveetha.com>
2026-02-12 10:32:02 +0530

    Create Hill Cipher Known-Plaintext Attack.c

---
+++ Padding in ECB /Hill Cipher Known-Plaintext Attack.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+ int P[2]={0,2};   
+ int C[2]={6,24};  
+ int K[2][2]={{3,3},{2,5}};  
+
+ printf("Key Matrix:\n%d %d\n%d %d",
+        K[0][0],K[0][1],K[1][0],K[1][1]);
+}

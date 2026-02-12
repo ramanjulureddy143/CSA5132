7639c83ecc3a6623da25172de0cbc5c4d0ff5a28
ramanjulureddy143 <ramanjulareddyu2123.sse@saveetha.com>
2026-02-12 10:34:01 +0530

    Create Monoalphabetic Cipher Frequency Attack.c

---
+++ Padding in ECB /Monoalphabetic Cipher Frequency Attack.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+ char c[]="QWERTY";
+ int f[26]={0}, i;
+
+ for(i=0;c[i];i++)
+   f[c[i]-'A']++;
+
+ for(i=0;i<26;i++)
+   if(f[i])
+     printf("%c : %d\n", i+'A', f[i]);
+}
